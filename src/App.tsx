d3eaeb36332ad87e7343d4a710ac9ba5e18969b0
Alexey <eaa04@bk.ru>
2026-04-11 17:19:34 +0300

    feat: integrate api

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
+import { GalleryPage } from "./pages/GalleryPage/GalleryPage";
+import { FavoritesPage } from "./pages/FavoritesPage/FavoritesPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route 
-          index 
-          element={<h1>Gallery</h1>} 
-        />
-        
-        <Route 
-          path="favorites"
-          element={<h1>Favourites</h1>} 
-        />
-        
+        <Route index element={<GalleryPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+
         <Route path="*" element={<h1>Страница не найдена</h1>} />
       </Route>
     </Routes>
