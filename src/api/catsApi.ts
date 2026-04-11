c31fe4361d4191be1ec7177bd7f5f876ed68a717
Alexey <eaa04@bk.ru>
2026-04-11 17:17:31 +0300

    feat: add redux store, create cats slice and hooks

--- src/api/catsApi.ts
+++ src/api/catsApi.ts
@@ -2,9 +2,11 @@ import { axiosInstance } from "./axiosInstance";
 import { ENDPOINTS } from "./endpoints";
 import { ICatImage } from "../types/cats.types";
 
+export const DEFAULT_CATS_LIMIT = 20;
+
 export const fetchCats = async (
   page: number = 0,
-  limit: number = 10,
+  limit: number = DEFAULT_CATS_LIMIT,
   order: "ASC" | "DESC" = "ASC",
 ): Promise<ICatImage[]> => {
   try {
---
+++ src/store/slices/catsSlice.ts
@@ -0,0 +1,69 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../hooks";
+import { ICatImage } from "../../types/cats.types";
+import { DEFAULT_CATS_LIMIT, fetchCats } from "../../api/catsApi";
+
+interface CatsState {
+  cats: ICatImage[];
+  loading: boolean;
+  error: string | null;
+  page: number;
+  hasMore: boolean;
+}
+
+const initialState: CatsState = {
+  cats: [],
+  loading: false,
+  error: null,
+  page: 0,
+  hasMore: true,
+};
+
+export const fetchCatsThunk = createAsyncThunk(
+  "cats/fetchCats",
+  async (page: number, { rejectWithValue }) => {
+    try {
+      const data = await fetchCats(page);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return data as ICatImage[];
+    } catch (error) {
+      return rejectWithValue(`fetchCatsThunk error: ${error}`);
+    }
+  },
+);
+
+const catsSlice = createSlice({
+  name: "cats",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCatsThunk.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchCatsThunk.fulfilled,
+        (state, action: PayloadAction<ICatImage[]>) => {
+          state.loading = false;
+          state.cats.push(...action.payload);
+          state.page += 1;
+
+          if (action.payload.length < DEFAULT_CATS_LIMIT) {
+            state.hasMore = false;
+          }
+        },
+      )
+      .addCase(fetchCatsThunk.rejected, (state, action) => {
+        state.loading = false;
+        state.error = (action.payload as string) || "Unknown error";
+      });
+  },
+});
+
+export const selectCats = (state: RootState) => state.cats.cats;
+export const selectCatsLoading = (state: RootState) => state.cats.loading;
+export const selectCatsError = (state: RootState) => state.cats.error;
+export const selectHasMore = (state: RootState) => state.cats.hasMore;
+
+export default catsSlice.reducer;
